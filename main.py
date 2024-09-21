8f22c4917840b7e705387917bf591768d348f164
Xomniacs <revannovanda2005@gmail.com>
2024-09-21 15:15:55 +0700

    Latihan coding 21 Sep 2024, hamba Allah

---
+++ main.py
@@ -0,0 +1,41 @@
+# nilai = int(input("masukkan nilai: "))
+
+# if (nilai > 80):
+#     print("Selamat anda lulus")
+# elif (nilai > 90 and nilai <= 100):
+#     print("Selamat anda lulus, dengan nilai yanng memuaskan")
+# else:
+#     print("Anda masih belum lulus tes, semangat dan coba lagi!")
+# elif (nilai > 70):
+#     print("Anda lulus tapi pinggir jurang")
+
+#[LOOP]
+# for loop
+# for i in range(10, 0, -2):
+#     print(i)
+#     print("hello")
+
+# angka = 30
+
+# while (angka > 1):
+#     print(angka)
+#     angka -= 1
+
+#[FUNCTION]
+# def sayHello():
+#     print("Hello wow")
+#     print("Hebat")
+
+# sayHello()
+# sayHello()
+# sayHello()
+
+# def sayHello(param_meter):
+#     print(f"Hello {param_nama}")
+#     print("Hebat")
+
+#[LAMBDA]
+
+fungsi_2kali2 = 2 * 2
+
+print(fungsi_2kali2)
